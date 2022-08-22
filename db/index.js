f1626d4fe281e18c32518c0ba638e6837b34e2fe
Enrique Orta <enriqueorta1994@gmail.com>
2022-08-22 11:14:18 -0400

    database

--- db/index.js
+++ db/index.js
@@ -4,11 +4,12 @@ const mongoose = require("mongoose");
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
-
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/proyectofinal";
-
+const username ="admin123"
+const password = "kike123"
+//const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/proyectofinal";
+const uri = /*"mongodb://localhost/proyectofinal"*/`mongodb+srv://${username}:${password}@cluster0.v4wpnf2.mongodb.net/proyectofinal?retryWrites=true&w=majority`;
 mongoose
-  .connect(MONGO_URI)
+  .connect(uri)
   .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
@@ -16,4 +17,5 @@ mongoose
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
-  });
+  }); 
+
